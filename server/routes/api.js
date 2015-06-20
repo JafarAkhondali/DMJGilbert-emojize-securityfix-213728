5a579e8ac7aaa2976e41bbe0765b94fcddd50bb0
Darren Gilbert <dmjgilbert@me.com>
2015-06-20 23:09:00 +0100

    Added sentiment and video api

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,6 +3,9 @@ var youtube = require('youtube-dl');
 var mkdirp = require('mkdirp');
 var rest = require('restler');
 var async = require('async');
+var iod = require('iod-node')
+
+var client = new iod.IODClient('http://api.idolondemand.com', 'ca566e83-6d86-4c7a-ae83-7e9a38d54e6f')
 
 var CACHE_DIR = './server/cache/';
 var YT_PREFIX = 'https://www.youtube.com/watch?v=';
@@ -19,7 +22,7 @@ module.exports = function (app) {
 		if (url) {
 			if (isCached(url)) {
 				fs.readFile(CACHE_DIR + url + "/data.json", 'utf8', function (err, data) {
-					async.map(JSON.parse(data), function (d, cb){
+					async.map(JSON.parse(data), function (d, cb) {
 						if (d.int_response == 1) {
 							rest.post('http://betafaceapi.com/service_json.svc/GetImageInfo', {
 								multipart: true,
@@ -31,7 +34,7 @@ module.exports = function (app) {
 							}).on('complete', function (newData, response) {
 								cb(null, newData)
 							});
-						}else{
+						} else {
 							cb(null, d);
 						}
 					}, function (err, results) {
@@ -50,6 +53,65 @@ module.exports = function (app) {
 			});
 		}
 	});
+
+	app.get('/api/sentiment', function (req, res) {
+		var url = req.query.url;
+		if (url) {
+			if (isCached(url)) {
+				if (fs.existsSync(CACHE_DIR + url + "/sentiment.json")) { //CACHE_DIR + ytid + "/sentiment.json"
+					fs.readFile(CACHE_DIR + url + "/sentiment.json", 'utf8', function (err, data) {
+						res.jsonp(JSON.stringify(data));
+					});
+				} else {
+					var data = {
+						'file': CACHE_DIR + url + '/video.mp4'
+					};
+					console.log(data)
+					client.call('recognizespeech', data, function (err, resp, body) {
+						rest.post('http://api.idolondemand.com/1/job/result/' + body.data.jobID, {
+							multipart: true,
+							data: {
+								apikey: 'ca566e83-6d86-4c7a-ae83-7e9a38d54e6f'
+							}
+						}).on('complete', function (magic, response) {
+							var text = magic.actions[0].result.document[0].content;
+							client.call('analyzesentiment', {
+								text: text
+							}, function (err, resp, body) {
+								fs.writeFile(CACHE_DIR + url + "/sentiment.json", JSON.stringify(body), function (err) {});
+								res.jsonp(JSON.parse(data));
+							})
+						});
+					}, true);
+				}
+			} else {
+				res.jsonp({
+					error: 'Video not cached'
+				});
+			}
+		} else {
+			res.jsonp({
+				error: 'Unsupported URL'
+			});
+		}
+	})
+
+	app.get('/api/video', function (req, res) {
+		var url = req.query.url;
+		if (url) {
+			if (isCached(url)) {
+				res.sendfile(CACHE_DIR + url + '/video.mp4');
+			} else {
+				res.jsonp({
+					error: 'Video not cached'
+				});
+			}
+		} else {
+			res.jsonp({
+				error: 'Unsupported URL'
+			});
+		}
+	})
 };
 
 function isCached(ytid) {
