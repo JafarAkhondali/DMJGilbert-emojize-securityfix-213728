4e15e0b1f810919a348eb916e2ebb661e9fa11d3
Matt R <matthieu@Matth.local>
2015-06-21 07:48:27 +0100

    reorganised code structure a bit

--- public/js/main.js
+++ public/js/main.js
@@ -64,69 +64,6 @@ $(function() {
         
     });
     
-    $("#video-submit").on("click", function(e){
-        e.preventDefault();
-        var videoInput = $("#vid-input").val();
-        console.log(videoInput);
     
-        var splitCriteria = "?v=";
-        var videoUrl = videoInput.substring(videoInput.indexOf("?v=") + splitCriteria.length, videoInput.length);
-        
-        console.log(videoUrl);
-        
-        queryAPI(videoUrl);
-    });
-    
-    
-    function queryAPI(url){
-        $.getJSON('api/youtube', {
-            url: url
-        }).done(function(data){
-            checkStatus(data);
-        });
-    }
-    
-
-  
-    function checkStatus(data){
-        console.log("done"); 
-        //console.log(data);
-        var string = "eye";
-        var target, name, x, y;
-        var eye = {};
-        for(var i = 0; i < data.length; i++){
-            if(data[i].faces.length > 0){
-                //console.log(data[i].faces);
-                for(var j = 0; j < data[i].faces.length; j++){
-                    if(data[i].faces[j].points.length > 0){
-                        //console.log(data[i].faces[j].points);
-                        
-                        for(var k = 0; k < data[i].faces[j].points.length; k++){
-                            
-                            target = data[i].faces[j].points[k];
-                            name = target.name;
-                            x = target.x;
-                            y = target.y;
-                            
-                            
-                            if(name.indexOf("eye right") !== -1){
-                                //console.log("hello right: ", name, x, y);
-                                //target.x
-                                eye.rightX = target.x;
-                                eye.rightY = target.y;
-                            }
-                            
-                            if(name.indexOf("eye left") !== -1){
-                                //console.log("hello left: ", name, x, y);
-                                eye.leftX = target.x;
-                                eye.leftY = target.y;
-                            }
-                            eyes.push(eye);
-                        }
-                    }
-                }
-            }
-        }
-    }
     
 })
